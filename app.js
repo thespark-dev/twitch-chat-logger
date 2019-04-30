ff97f738a02e89ce9bde52e9bd3827180b06831f
Ezra <50077019+theeSpark@users.noreply.github.com>
2019-04-30 16:43:07 +0200

    :)

---
+++ app.js
@@ -0,0 +1,71 @@
+const tfapi = require('tmi.js');
+const fku = require('./config.json');
+const fs = require('fs');
+
+var joinchnl = '';
+
+const thefuckingoptions = {
+	options: {
+		debug: false
+	},
+	connection: {
+		cluster: 'aws',
+		reconnect: true
+	},
+	identity: {
+		username: 'theeSparkBot', // Bot Username
+		password: fku.token
+	},
+	channels: [ joinchnl ]
+};
+
+const client = new tfapi.client(thefuckingoptions);
+client.connect();
+
+/* LOGGER */
+var stream;
+stream = fs.createWriteStream("./log.txt");
+
+client.on('chat', (channel,user,message,self) => {
+	let sender = user['display-name'];
+
+	var time = () => {
+		var d = new Date();
+
+		var datetext = d.toTimeString();
+
+		datetext = datetext.split(' ')[0];
+
+		return datetext;
+	}
+
+	if (sender.toLowerCase().includes("bot")) {
+		stream.write(`${time()} - [BOT] ${sender} | ${message}\n`);
+	}
+
+	else if (sender.toLowerCase() == "theespark" || sender.toLowerCase() == joinchnl) {
+		stream.write(`${time()} - [KING] ${sender} | ${message}\n`);
+	} else {
+		stream.write(`${time()} - ${sender} | ${message}\n`);
+	}
+
+})
+
+client.on('action', (channel,user,message,self) => {
+	let sender = user['display-name'];
+
+	if (sender.toLowerCase().includes("bot")) {
+		stream.write(`${time()} - * [BOT] ${sender} | ${message}\n`);
+	}
+
+	else if (sender.toLowerCase() == "theespark" || sender.toLowerCase() == joinchnl) {
+		stream.write(`${time()} - * [KING] ${sender} | ${message}\n`);
+	} else {
+		stream.write(`${time()} - * ${sender} | ${message}\n`);
+	}
+})
+
+client.on('timeout', (channel, username, reason, duration, user) =>{
+	stream.write(`${time()} - [TIMEOUT] ${user["target-user-id"]} timeouted af ${username} i ${duration} sekunder.`);
+})
+/* LOGGER */
